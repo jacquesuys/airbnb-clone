d99ac5e207013c66ded95444079477e40a0aac94
Jacques Uys <jaxuys@gmail.com>
2022-12-11 15:09:05 +0200

    Homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,12 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Header from "../components/Header";
 import Banner from "../components/Banner";
+import SmallCard from "../components/SmallCard";
+import MediumCard from "../components/MediumCard";
+import LargeCard from "../components/LargeCard";
+import Footer from "../components/Footer";
 
-const Home: NextPage = () => {
+const Home: NextPage = ({ exploreData, cardsData }: any) => {
   return (
     <div className="">
       <Head>
@@ -13,8 +17,60 @@ const Home: NextPage = () => {
 
       <Header />
       <Banner />
+
+      <main className="max-w-7xl mx-auto px-8 sm:px-16">
+        <section className="pt-6">
+          <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            {exploreData?.map(
+              ({ img, distance, location }: any, key: number) => (
+                <SmallCard
+                  key={key}
+                  location={location}
+                  img={img}
+                  distance={distance}
+                />
+              )
+            )}
+          </div>
+        </section>
+
+        <section className="pt-6 p-3 -ml-3">
+          <h2 className="text-4xl font-semibold pb-5">Live anywhere</h2>
+          <div className="flex space-x-3 p-3 -ml-3 overflow-scroll scrollbar-hide">
+            {cardsData?.map(({ img, title }: any, key: number) => (
+              <MediumCard key={key} title={title} img={img} />
+            ))}
+          </div>
+        </section>
+
+        <LargeCard
+          img="https://a0.muscache.com/im/pictures/2da67c1c-0c61-4629-8798-1d4de1ac9291.jpg?im_w=1440"
+          title="The Greatest Outdoors"
+          description="A wishlist curated by AirBnB"
+          buttonText="Get Inspired"
+        />
+      </main>
+      <Footer />
     </div>
   );
 };
 
 export default Home;
+
+export async function getStaticProps() {
+  const exploreData = await fetch("https://www.jsonkeeper.com/b/4G1G").then(
+    (res) => res.json()
+  );
+
+  const cardsData = await fetch("https://www.jsonkeeper.com/b/VHHT").then(
+    (res) => res.json()
+  );
+
+  return {
+    props: {
+      exploreData,
+      cardsData,
+    },
+  };
+}
